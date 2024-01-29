e1c616990c88695442f95edbf7565ad2dc47c5d7
Ujjawal-Chaurasia <chaurasiaujjawal208@gmail.com>
2024-01-29 11:10:18 +0530

    first commit

--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -15,7 +15,7 @@ const LoginForm = () => {
       <button>Login</button>
       {state?.error}
       <Link href="/register">
-        {"Don't have an account?"} <b>Register</b>
+        <p style={{"color":'#000'}}>{"Don't have an account?"} <b>Register</b></p>
       </Link>
     </form>
   );
